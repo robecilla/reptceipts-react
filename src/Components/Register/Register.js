742396f629e9848d29166bd896bcb456a1996467
Roberto Vecilla <roberto.vecilla@gmail.com>
2017-12-27 23:19:28 +0000

    changed font and phone render in landing page

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,28 +1,40 @@
 import React, { Component } from 'react';
 // Components
-import { HeroBody,Title, Container, Columns, Column } from 'bloomer';
+import { HeroBody, Title, Subtitle, Container, Columns, Column, Image } from 'bloomer';
 import RegisterForm from './RegisterForm';
+
+import s8 from '../../Assets/img/s8-render.png';
+import gplay from '../../Assets/img/googleplay.png';
+
+const s8position = {
+	'width': '70%'
+}
+
+const playpositon = {
+	'bottom' : '70px',
+	'left' : '50px'
+}
 
 class Register extends Component {
 
   render() {
     return (
-	<HeroBody>
+	<HeroBody style={{ 'alignItems' : 'initial' }}>
+		<Container>
+			<Columns isMultiline>
+				<Column isSize={{ mobile: 'full', desktop: 'full' }} isOffset={{ 'desktop' : 2}}>
+					<Title isSize={3}>Keep your purchase receipts organised</Title>
+					<Subtitle isSize={6} style={{'marginTop' : '10px' }}> reptceipts helps you organise, manage and redeem your purchase receipts </Subtitle>
+				</Column>
+				<Column isSize={{ mobile: 'full', desktop: 6 }} isOffset={{ 'desktop' : 2}}>
+					<RegisterForm />
+				</Column>
+			</Columns>
+		</Container>
 		<Container>
 			<Columns>
-				<Column isSize={{ mobile: 'full', desktop: 3 }}>
-					<Title isSize={3} className={'has-text-weight-light'}>Create an account</Title>
-
-				{ /* RegisterForm component, passing parameters depending on the kind of registration? */ }
-
-					<RegisterForm />
-
-				</Column>
-				<Column isSize={{ desktop: 4 }} isHidden='mobile'>
-
-				</Column>
-				<Column isSize={{ desktop: 4 }} isHidden='mobile'>
-
+				<Column isSize={{ desktop : 9 }} isOffset={{ 'desktop' : 3 }}>
+					<Image src={s8} style={s8position} />
 				</Column>
 			</Columns>
 		</Container>
