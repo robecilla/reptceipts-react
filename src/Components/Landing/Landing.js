c47e6bd5bfcc3b08d2040c122a8f034c6c2f1954
Roberto Vecilla <roberto.vecilla@gmail.com>
2017-12-28 11:44:51 +0000

    renamed components

---
+++ src/Components/Landing/Landing.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+// Components
+import {
+  HeroBody,
+  Title,
+  Subtitle,
+  Container,
+  Columns,
+  Column,
+  Image
+} from 'bloomer';
+
+import RegisterForm from '../Register/RegisterForm';
+
+import s8 from '../../Assets/img/s8-render.png';
+//import gplay from '../../Assets/img/googleplay.png';
+
+const s8position = {
+  width: '70%'
+};
+
+// const playpositon = {
+// 	'bottom' : '70px',
+// 	'left' : '50px'
+// }
+
+class Register extends Component {
+  render() {
+    return (
+      <HeroBody style={{ alignItems: 'initial' }}>
+        <Container>
+          <Columns isMultiline>
+            <Column
+              isSize={{ mobile: 'full', desktop: 'full' }}
+              isOffset={{ desktop: 2 }}
+            >
+              <Title isSize={3}>Keep your purchase receipts organised</Title>
+              <Subtitle isSize={6} style={{ marginTop: '10px' }}>
+                {' '}
+                reptceipts helps you organise, manage and redeem your purchase
+                receipts{' '}
+              </Subtitle>
+            </Column>
+            <Column
+              isSize={{ mobile: 'full', desktop: 6, widescreen: 4 }}
+              isOffset={{ desktop: 2 }}
+            >
+              <RegisterForm />
+            </Column>
+          </Columns>
+        </Container>
+        <Container>
+          <Columns>
+            <Column
+              isSize={{ mobile: 'full', desktop: 9 }}
+              isOffset={{ mobile: 3, desktop: 3 }}
+            >
+              <Image src={s8} style={s8position} />
+            </Column>
+          </Columns>
+        </Container>
+      </HeroBody>
+    );
+  }
+}
+
+export default Register;
--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-import { Hero, HeroHeader } from 'bloomer';
+import { Hero, HeroHeader, HeroBody, Title, Container } from 'bloomer';
 
 import Navbar from '../Components/Navbar/Navbar';
 import Main from '../Components/Main/Main';
 
 class Layout extends Component {
-
   render() {
     return (
-      <Hero isColor='dark' isFullHeight>
-        <HeroHeader>
-          <Navbar />
-        </HeroHeader>
-        <Main />
-      </Hero>
+      <div>
+        <Hero isColor="dark" isFullHeight>
+          <HeroHeader>
+            <Navbar />
+          </HeroHeader>
+
+          <Main />
+        </Hero>
+
+        <Hero isColor="light" isFullHeight>
+          <HeroBody>
+            <Container hasTextAlign="centered">
+              <Title isSize={3}>How it works, etc ... </Title>
+            </Container>
+          </HeroBody>
+        </Hero>
+      </div>
     );
   }
 }
