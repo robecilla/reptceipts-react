ba68af5cd7fd1126fbeedf4bdf0de7aefd16f09d
Roberto Vecilla <roberto.vecilla@gmail.com>
2017-12-28 11:47:12 +0000

    dependencies update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
-import 'bulma/css/bulma.css'
+import 'bulma/css/bulma.css';
 import './index.css';
 
 import Layout from './Layout/Layout';
@@ -11,8 +11,8 @@ import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(
   <BrowserRouter>
     <Layout />
-  </BrowserRouter>
-  , document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById('root')
 );
 
 registerServiceWorker();
