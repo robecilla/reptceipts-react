21c61616842704267ed372aeee2b34e8d7cf8ebc
robvecillaTUX <roberto.vecilla@tuxedomoney.com>
2018-01-30 19:15:03 +0000

    cleared state after log out

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -4,11 +4,18 @@ import authReducer from './authReducer';
 import UserReducer from './UserReducer';
 import UIState from './UIState';
 
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
   form: formReducer,
   auth: authReducer,
   user: UserReducer,
   ui: UIState
 });
+
+const rootReducer = (state, action) => {
+  if (action.type === 'UNAUTH_USER') {
+    state = undefined;
+  }
+  return appReducer(state, action);
+};
 
 export default rootReducer;
