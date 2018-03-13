a6c08aa9def8101090a0de366fd9a82e39d3b4ea
Roberto Vecilla <roberto.vecilla@gmail.com>
2018-03-13 17:45:13 +0000

    added first qr code example

--- src/Components/Menu/Receipts/QRExamples/QRExamples.js
+++ src/Components/Menu/Receipts/QRExamples/QRExamples.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 // Components
 import { Title, Column } from 'bloomer';
+import qr1 from '../../../../Assets/img/qr/json_receipt1_qr.png';
 
 class QRExamples extends Component {
   render() {
     return (
       <Column
         isSize={{ desktop: 10 }}
-        className="hero is-fullheight"
+        className="is-fullheight"
         style={{ padding: '40px 20px' }}
       >
         <Title isSize={3}>Example QR Codes</Title>
+        <img src={qr1} alt="receipt1" width="200" />
       </Column>
     );
   }
