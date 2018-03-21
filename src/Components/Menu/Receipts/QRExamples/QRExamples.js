261aea34dafa31607c30907f1f8ee562086ea950
Roberto Vecilla <robvecilla@robertos-mbp.home>
2018-03-21 14:31:59 +0000

    implmented qr codes generator first proto

--- src/Components/Menu/Receipts/QRExamples/QRExamples.js
+++ src/Components/Menu/Receipts/QRExamples/QRExamples.js
@@ -1,18 +1,67 @@
 import React, { Component } from 'react';
 // Components
-import { Title, Column } from 'bloomer';
-import qr1 from '../../../../Assets/img/qr/json_receipt1_qr.png';
+import { Title, Column, Field, Control, Label, Input, Button } from 'bloomer';
+
+// function isEmpty(obj) {
+//   for (var key in obj) {
+//     if (obj.hasOwnProperty(key)) return false;
+//   }
+//   return true;
+// }
+
+/* Imports qrcodes dynamicaly */
+// function importAll(r) {
+//   let codes = {};
+//   r.keys().map((item, index) => { codes[item.replace('./', '')] = r(item); });
+//   return codes;
+// }
+
+// const codes = importAll(require.context('../../../../Assets/img/qr'));
 
 class QRExamples extends Component {
+  constructor() {
+    super();
+    this.state = {
+      qr: false
+    };
+    this.generateQR = this.generateQR.bind(this);
+  }
+
+  generateQR(e) {
+    e.preventDefault();
+    const qrContent = new FormData(e.target).get('qrContent');
+    let qr = `https://chart.googleapis.com/chart?cht=qr&chl=${qrContent}&chs=300x300&chld=L|0"`;
+    this.setState({ qr: qr });
+  }
+
   render() {
     return (
       <Column
-        isSize={{ desktop: 10 }}
+        isSize={{ desktop: 4 }}
         className="is-fullheight"
         style={{ padding: '40px 20px' }}
       >
         <Title isSize={3}>Example QR Codes</Title>
-        <img src={qr1} alt="receipt1" width="200" />
+        <form onSubmit={this.generateQR}>
+          <Field>
+            <Label>Generate receipt</Label>
+            <Control>
+              <Input
+                type="text"
+                placeholder="Minifed JSON .."
+                name="qrContent"
+              />
+            </Control>
+          </Field>
+          <Field>
+            <Control>
+              <Button type="submit" isColor="warning">
+                Generate
+              </Button>
+            </Control>
+          </Field>
+        </form>
+        {this.state.qr ? <img src={this.state.qr} alt="qr" /> : false}
       </Column>
     );
   }
